457443bde1034d175c2effe28f4c44cea1ce399c
Nvethrandil <135549000+Nvethrandil@users.noreply.github.com>
2024-03-28 19:51:34 +0100

    Update utility.h
    opencv/cv.h --> opencv2/imgproc.hpp

--- traversability_mapping/include/utility.h
+++ traversability_mapping/include/utility.h
@@ -21,7 +21,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/core/eigen.hpp>
 
-#include <opencv/cv.h>
+#include <opencv2/imgproc.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
 
